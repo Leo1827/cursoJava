06ab2af245194d281f15292cb3d6998fba645cc5
Mileer leon <mileerdubanleon@gmail.com>
2025-10-20 15:47:58 -0500

    feat: structure for

---
+++ src/StructureFor.java
@@ -0,0 +1,8 @@
+public class StructureFor {
+    public static void main(String[] args) {
+        // For: el bucle determinado por contador, contar posiciones o cuestiones
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Estoy en la vuelta numero: " + (i+1));
+        }
+    }
+}
