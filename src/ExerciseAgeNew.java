3c6816297431d93efe2689ab44da886f7507391f
Mileer leon <mileerdubanleon@gmail.com>
2025-10-20 22:51:24 -0500

    feat: add exercise the age new

---
+++ src/ExerciseAgeNew.java
@@ -0,0 +1,9 @@
+public class ExerciseAgeNew {
+    public static void main(String[] args) {
+        System.out.println("Cuenta regresiva año nuevo");
+        for (int i=10;i>=0;i--){
+            System.out.printf("Cuenta regresiva: %d\n",i);
+        }
+        System.out.println("¡Feliz Año Nuevo!");
+    }
+}
