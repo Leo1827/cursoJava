3fcb5f1aa14e2ffe59d691312c3460bf6546c035
Mileer leon <mileerdubanleon@gmail.com>
2025-10-20 22:16:52 -0500

    feat: exercise basic numbers

---
+++ src/ExerciseNumbers.java
@@ -0,0 +1,8 @@
+public class ExerciseNumbers {
+    public static void main(String[] args) {
+
+        for (int i=1; i <= 35; i++){
+            System.out.printf("Numero: %d\n",i);
+        }
+    }
+}
