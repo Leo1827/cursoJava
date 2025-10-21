61d979b2974e09a7f759af9c87e4e140378f4486
Mileer leon <mileerdubanleon@gmail.com>
2025-10-20 22:38:57 -0500

    feat: add exercise for number 2

---
+++ src/ExerciseDoscientos.java
@@ -0,0 +1,8 @@
+public class ExerciseDoscientos {
+    public static void main(String[] args) {
+
+        for (int i=200;i<=250;i+=2){
+            System.out.printf("Numero: %d\n",i);
+        }
+    }
+}
