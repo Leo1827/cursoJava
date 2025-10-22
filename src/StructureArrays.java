20de946a71b99d2b899df9665cd4e2906ae3a865
Mileer leon <mileerdubanleon@gmail.com>
2025-10-21 22:12:55 -0500

    feat: add section in arrays with user scanner

--- src/StructureArrays.java
+++ src/StructureArrays.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class StructureArrays {
     public static void main(String[] args) {
         // los arreglos son un conjunto de datos que se almacena enmemoria de manera contigua
@@ -6,26 +8,32 @@ public class StructureArrays {
         // cada caja es una posicion de un arreglo
         // tamaño fijo, una vez declarada no se puede cambiar el tamaño
 
-        // Arreglos Unidimensional: poseen solo filas o solo columnas ejem: los vectores
+        // **Arreglos Unidimensional: poseen solo filas o solo columnas ejem: los vectores
         // vector
         int numeros [] = new int [8];// declaro e inicializo mi arreglo
-        numeros [0] = 35;
+        Scanner sc = new Scanner(System.in);
+        /*numeros [0] = 35;
         numeros [1] = 25;
         numeros [2] = 16;
         numeros [3] = 30;
         numeros [4] = 40;
         numeros [5] = 50;
         numeros [6] = 22;
-        numeros [7] = 32;
+        numeros [7] = 32;*/
 
-        System.out.println(numeros[4]);
         // en un vector lo importante es la carga y el recorrido
         // length agrega el valor de indices de numeros
         for(int i=0;i<numeros.length;i++){
-            System.out.println("Estoy en el indice: " + i + "Y el valor es: " + numeros[i]);
+            // System.out.println("Estoy en el indice: " + i + "Y el valor es: " + numeros[i]);
+            System.out.println("Ingrese el valor para el indice: " + i);
+            numeros[i] = sc.nextInt();
+        }
+        System.out.println("Los valores ingresados fueron");
+        for(int i=0; i<numeros.length;i++){
+            System.out.println("Indice: "+ i + "Valor: " + numeros);
         }
 
-        // Array Bidimensional: Forman tablas de valores, donde existen dos dimensiones ejemp: matrices
+        // **Array Bidimensional: Forman tablas de valores, donde existen dos dimensiones ejemp: matrices
 
     }
 }
