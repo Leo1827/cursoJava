fcebbec531f4b9b5ad2334cf4f85df0590a4c33e
Mileer leon <mileerdubanleon@gmail.com>
2025-10-21 22:49:13 -0500

    feat: add matriz no cuadrada and length: cantidad de valores segun el indice

--- src/StructureArrayMatrices.java
+++ src/StructureArrayMatrices.java
@@ -6,8 +6,8 @@ public class StructureArrayMatrices {
         // Se manejan por columnas y filas
         // Cuando se tiene la misma cantidad de filas y columnas, se le denomina: **Matriz cuadrada**
 
-        // matriz cuadrada
-        int matriz [][] = new int [3][3]; // primero filas y luego las columnas
+        // ******* Matriz cuadrada
+        /*int matriz [][] = new int [3][3]; // primero filas y luego las columnas
         Scanner sc = new Scanner(System.in);
 
         // doble for: un for que va recorriendo cada una de las filas
@@ -26,6 +26,26 @@ public class StructureArrayMatrices {
             for(int c=0;c<3;c++) {
                 System.out.println("Fila: " + f + "Columna " + c + "Valor" + matriz[f][c]);
             }
+        }*/
+
+        // Length: longitud Saber cuantos elemento tiene un arreglo, devuelve el tamaño del arreglo
+
+        // ********* Cuando no es cuadrada
+        int matriz [][] = new int [3][5]; // primero filas y luego las columnas
+        Scanner sc = new Scanner(System.in);
+
+        for(int f=0;f<matriz.length; f++){ // me devuelve la cantidad de filas
+            for(int c=0;c<matriz[0].length;c++) { // me cuenta las columnas de la fila 0
+                System.out.println("Ingrese el valor para la fila: " + f + "Columna " + c);
+                matriz[f][c] = sc.nextInt();
+            }
         }
+
+        for(int f=0;f<matriz.length; f++){
+            for(int c=0;c<matriz[0].length;c++) {
+                System.out.println("Fila: " + f + "Columna " + c + "Valor" + matriz[f][c]);
+            }
+        }
+
     }
 }
